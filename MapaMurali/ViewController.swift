cfe697aaf1a9c6afca5bf7a56d045417ce87722b
Jakub Zajda <jakubzajda@gmail.com>
2022-07-28 20:48:38 +0200

    Initial Commit

---
+++ MapaMurali/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapaMurali
+//
+//  Created by Jakub Zajda on 28/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
