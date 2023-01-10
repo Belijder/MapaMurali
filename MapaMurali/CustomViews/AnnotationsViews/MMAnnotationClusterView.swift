336bf556b31d6d51d86878b3718ed63d38231cba
Jakub Zajda <jakubzajda@gmail.com>
2023-01-10 16:22:57 +0100

    fix: fix another bug with changing appearance mode in MMAnnotationClusterView

--- MapaMurali/CustomViews/AnnotationsViews/MMAnnotationClusterView.swift
+++ MapaMurali/CustomViews/AnnotationsViews/MMAnnotationClusterView.swift
@@ -9,46 +9,57 @@ import UIKit
 import MapKit
 
 final class MMAnnotationClusterView: MKAnnotationView {
-
-    private let countLabel = UILabel()
     
+    //MARK: - Properties
+    private let countLabel = UILabel()
     static let reuseIdentifier = "MMAnnotationCluserView"
     
     override var annotation: MKAnnotation? {
         willSet {
-            if let annotation = annotation as? MKClusterAnnotation {
-                
-                let renderer = UIGraphicsImageRenderer(size: CGSize(width: 40, height: 40))
-                let count = annotation.memberAnnotations.count
-                
-                image = renderer.image(actions: { _ in
-                    MMColors.primary.setFill()
-                    UIBezierPath(ovalIn: CGRect(x: 0, y: 0, width: 40, height: 40)).fill()
-                    
-                    let attributes = [
-                        NSAttributedString.Key.foregroundColor: UIColor.white,
-                        NSAttributedString.Key.font: UIFont.systemFont(ofSize: 20, weight: .medium)]
-                    
-                    let text = "\(count)"
-                    let size = text.size(withAttributes: attributes)
-                    let rect = CGRect(x: 20 - size.width / 2, y: 20 - size.height / 2, width: size.width, height: size.height)
-                    text.draw(in: rect, withAttributes: attributes)
-                })
-            }
+            drawAnnotationView(for: self.annotation)
         }
     }
     
+    //MARK: - Live cicle
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        if traitCollection.hasDifferentColorAppearance(comparedTo: previousTraitCollection) {
+            drawAnnotationView(for: self.annotation)
+        }
+    }
+    
+    //MARK: - Logic
+    func drawAnnotationView(for annotation: MKAnnotation?) {
+        if let annotation = annotation as? MKClusterAnnotation {
+            
+            let renderer = UIGraphicsImageRenderer(size: CGSize(width: 40, height: 40))
+            let count = annotation.memberAnnotations.count
+            
+            image = renderer.image(actions: { _ in
+                MMColors.primary.setFill()
+                UIBezierPath(ovalIn: CGRect(x: 0, y: 0, width: 40, height: 40)).fill()
+                
+                let attributes = [
+                    NSAttributedString.Key.foregroundColor: UIColor.white,
+                    NSAttributedString.Key.font: UIFont.systemFont(ofSize: 20, weight: .medium)]
+                
+                let text = "\(count)"
+                let size = text.size(withAttributes: attributes)
+                let rect = CGRect(x: 20 - size.width / 2, y: 20 - size.height / 2, width: size.width, height: size.height)
+                text.draw(in: rect, withAttributes: attributes)
+            })
+        }
+    }
+    
+    //MARK: - Initialization
     override init(annotation: MKAnnotation?, reuseIdentifier: String?) {
         super.init(annotation: annotation, reuseIdentifier: reuseIdentifier)
         
         displayPriority = .defaultHigh
         collisionMode = .circle
         centerOffset = CGPoint(x: 0, y: -frame.size.height / 2)
-
     }
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
 }
